1b9161b8ba9ca285fb159a4123095b7cc0c64d54
99bomboclat99-sketch <99bomboclat99@gmail.com>
2026-09-02 14:00:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,3 @@
    فقط anon public key. */
 window.SUPABASE_URL = "https://woccikpdwpwsegtufpam.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvY2Npa3Bkd3B3c2VndHVmcGFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNDAwNDUsImV4cCI6MjEwMzkxNjA0NX0.dmGhSmr_9p1jeeuJhB3LY6eYRq3vvmPSKryJwdtXKoY
-";
