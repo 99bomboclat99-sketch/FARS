dd5847fdc9b29b66fc5c8012c11103147ed0cf95
99bomboclat99-sketch <99bomboclat99@gmail.com>
2026-09-02 14:35:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
    اللي تستخدمها بتطبيق أرلا الأصلي. لا تضع مفتاح service_role هنا،
    فقط anon public key. */
 window.SUPABASE_URL = "https://woccikpdwpwsegtufpam.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvY2Npa3Bkd3B3c2VndHVmcGFtIiwucm9sZSI6ImFub24iLCJpYXQiOi4uLg==";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvY2Npa3Bkd3B3c2VndHVmcGFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNDAwNDUsImV4cCI6MjEwMzkxNjA0NX0.dmGhSmr_9p1jeeuJhB3LY6eYRq3vvmPSKryJwdtXKoY";
